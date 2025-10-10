de6c67fdf7eeccc2db612fe48d44529c3e6a8a67
Redeter <rosenbzx@gmail.com>
2025-10-10 23:26:56 +0400

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 
-const API_URL = 'https://production-system.onrender.com';
+const API_URL = 'https://production-system.onrender.com/api';
 
 export const generateValues = async () => {
-    const res = await axios.get(`${API_URL}/generate`);
+    const res = await axios.get(`${API_URL}/generate-values`);
     return res.data;
 }
 
 export const runSystem = async (data) => {
-    const res = await axios.post(`${API_URL}/run`, data);
+    const res = await axios.post(`${API_URL}/calculate`, data);
+    return res.data;
+}
+
+export const loadDefaultCoefficients = async () => {
+    const res = await axios.get(`${API_URL}/default-coefficients`);
     return res.data;
 }
