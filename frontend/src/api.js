751f9fb6b476f54f8a32759e9df914b897ac02d5
Redeter <rosenbzx@gmail.com>
2025-10-10 23:12:43 +0400

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://production-system.onrender.com';
 
 export const generateValues = async () => {
     const res = await axios.get(`${API_URL}/generate`);
