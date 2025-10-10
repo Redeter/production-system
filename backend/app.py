026af18698891524d2afbf9b524aee63cd89fc04
Redeter <rosenbzx@gmail.com>
2025-10-10 21:11:23 +0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -83,8 +83,5 @@ def default_coefficients():
         return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
-    import os
-    if os.environ.get('RENDER'):
-        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=False)
-    else:
-        app.run(debug=True, host='127.0.0.1', port=5000)
+    
+    app.run(debug=True, host='127.0.0.1', port=5000)
