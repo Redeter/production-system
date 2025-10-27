abf817b95a994fcc609c6649b809c801e1bbc3e8
elixirofjoy <jaintjoy12@gmail.com>
2025-10-28 02:39:32 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -44,7 +44,7 @@ except ImportError as e:
     calculator = CalculatorStub()
 
 app = Flask(__name__, static_folder='static')
-CORS(app, origins=["http://localhost:3000", "http://127.0.0.1:5000", "http://localhost:5000"])
+CORS(app)
 
 # -------------------
 # API маршруты
