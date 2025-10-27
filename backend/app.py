8911fe962e671c061923bd9dd41590f01edd102d
Redeter <rosenbzx@gmail.com>
2025-10-28 00:33:53 +0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -44,7 +44,7 @@ except ImportError as e:
     calculator = CalculatorStub()
 
 app = Flask(__name__, static_folder='static')
-CORS(app, origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5000"])
+CORS(app, origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://production-system.onrender.com"])
 
 # -------------------
 # API маршруты
