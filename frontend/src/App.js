53861103225d150fe24820f9526542af0de4f897
Redeter <rosenbzx@gmail.com>
2025-10-28 00:06:58 +0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import ResultsTab from './components/ResultsTab';
 import './App.css';
 
 // Базовый URL для API
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'process.env.REACT_APP_API_URL';
 
 function App() {
   const [activeTab, setActiveTab] = useState('parameters');
